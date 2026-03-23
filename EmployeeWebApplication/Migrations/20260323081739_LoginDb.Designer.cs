c5c38e3427e2302261f9c3fb17d3907d6308eccd
sohanlandge <sohanlandge20@gmail.com>
2026-03-23 13:49:28 +0530

    Migration

--- EmployeeWebApplication/Migrations/20260323081739_LoginDb.Designer.cs
+++ EmployeeWebApplication/Migrations/20260323081739_LoginDb.Designer.cs
@@ -4,6 +4,7 @@ using EmployeeWebApplication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeWebApplication.Migrations
 {
     [DbContext(typeof(EmployeeDBContext))]
-    partial class EmployeeDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260323081739_LoginDb")]
+    partial class LoginDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
