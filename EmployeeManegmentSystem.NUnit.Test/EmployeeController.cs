5a052ce1b1a384a53e50f5a2fdcf38e928c57ab0
sohanlandge <sohanlandge20@gmail.com>
2026-04-08 14:44:28 +0530

    updated

---
+++ EmployeeManegmentSystem.NUnit.Test/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeWebApplication.Data;
+using EmployeeWebApplication.Controllers;
+using EmployeeWebApplication.Models;
+using System.Collections.Generic;
+
+namespace EmployeeManegmentSystem.NUnit.Test
+{
+    public class EmployeeController
+    {
+
+    }
+}
---
+++ EmployeeWebApplication/Migrations/20260324070248_InitialLogdb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeWebApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialLogdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
